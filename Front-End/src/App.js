eb65e0664b07a4e590546426dc9558497e995e96
unknown <ritupowar79@gmail.com>
2024-02-05 15:40:52 +0530

    Registration and Homepage Header Front-End

---
+++ Front-End/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from  './components/Header';
+import Registration from './components/Registration'
+function App() {
+  return (
+    <div>
+        {/* <Header /> */}
+       
+        <Registration />
+    </div>
+   
+  );
+}
+
+export default App;
