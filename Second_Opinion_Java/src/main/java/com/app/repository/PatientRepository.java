9859384b42053e8272b9881d0f6d35e917514c8b
unknown <ritupowar79@gmail.com>
2024-02-14 19:59:06 +0530

    Added Patient post,get,delete functionality

--- Second_Opinion_Java/src/main/java/com/app/repository/PatientRepository.java
+++ Second_Opinion_Java/src/main/java/com/app/repository/PatientRepository.java
@@ -2,8 +2,8 @@ package com.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import com.app.entity.PatientEntity;
+
 
 public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
 
