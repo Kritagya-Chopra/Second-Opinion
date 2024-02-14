f5141912bbd9d343ab10809b9a77f78509bfdbbd
kd2-80139 <ujjwalpratapsingh01@gmail.com>
2024-02-14 13:01:11 +0530

    Navbar, addcases, patientDashboard, Login,routing created

---
+++ Second_Opinion_React/src/components/AddCase.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./AddCase.css"
+const AddCase=()=>{
+    return(
+        <>
+            <div className="headingAddCase">
+            <h2>Search for a specialist</h2>
+            <p>
+            To find the most suitable specialist for your case, please type a diagnosis, 
+            symptom or body part of concern:
+            </p>
+            </div>
+            
+            <input type="text" placeholder="Enter Disease"></input>
+            <button className="search">Search</button>
+        </>
+    )
+}
+export default AddCase;
---
+++ Second_Opinion_React/src/pages/PatientDashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import PatientComponent from "./PatientComponent";
+
+const Dashboard=()=>{
+    return(
+        <>
+            <Navbar/>
+            <PatientComponent/>
+        </>
+    )
+}
+export default Dashboard;
