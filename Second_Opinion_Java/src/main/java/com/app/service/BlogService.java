b0aea8d30d589c4ae5abfa316fc2ff00ed2c0217
kd2-80139 <ujjwalpratapsingh01@gmail.com>
2024-02-15 15:03:41 +0530

    blogs all functionality completed

--- Second_Opinion_Java/src/main/java/com/app/service/BlogService.java
+++ Second_Opinion_Java/src/main/java/com/app/service/BlogService.java
@@ -2,9 +2,13 @@ package com.app.service;
 
 import java.util.List;
 
-import com.app.entity.BlogEntity;
+import com.app.dto.BlogDTO;
+
 
 public interface BlogService {
-	public List<BlogEntity> getAllBlogs(Long id);
-	public BlogEntity add(BlogEntity b,Long id);
+	public List<BlogDTO> getAllBlogs(Long id);
+	public BlogDTO add(BlogDTO b,Long id);
+	public List<BlogDTO> allBlogs();
+	public Boolean deleteBlog(Long id);
+	public BlogDTO updateBlog(Long id, BlogDTO blog);
 }
--- Second_Opinion_Java/src/main/java/com/app/service/BlogServiceImpl.java
+++ Second_Opinion_Java/src/main/java/com/app/service/BlogServiceImpl.java
@@ -1,11 +1,15 @@
 package com.app.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.app.custom_exceptions.ResourceNotFoundException;
+import com.app.dto.BlogDTO;
 import com.app.entity.BlogEntity;
 import com.app.entity.DoctorEntity;
 import com.app.repository.BlogRepository;
@@ -13,22 +17,66 @@ import com.app.repository.DoctorRepository;
 @Service
 @Transactional
 public class BlogServiceImpl implements BlogService {
+	@Autowired 
+	private ModelMapper mapper;
 	@Autowired
 	private BlogRepository blogRepository;
 	@Autowired 
 	private DoctorRepository doctorRepository;
 	@Override
-	public List<BlogEntity> getAllBlogs(Long id) {
+	public List<BlogDTO> getAllBlogs(Long id) {
 		// TODO Auto-generated method stub
-		
-		return null;//blogRepository.findById(id);
+		DoctorEntity d=doctorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Doctor Not Found"));
+		List<BlogEntity> blogs=blogRepository.findByDoctorId(id);
+		List<BlogDTO> bDto=new ArrayList<BlogDTO>();
+		for (BlogEntity b : blogs) {
+			bDto.add(mapper.map(b, BlogDTO.class));
+		}
+		for (BlogDTO b : bDto) {
+			b.setName(d.getName());
+		}
+		return bDto;
 	}
 	@Override
-	public BlogEntity add(BlogEntity b,Long id) {
+	public BlogDTO add(BlogDTO b,Long id) {
 		// TODO Auto-generated method stub
 		DoctorEntity d=doctorRepository.findById(id).orElseThrow();
-		b.setDoctor(d);
-		return blogRepository.save(b);
+		BlogEntity blog=mapper.map(b,BlogEntity.class);
+		d.addBlog(blog);
+		b.setName(d.getName());
+		blogRepository.save(blog);
+		return b;
+	}
+	@Override
+	public List<BlogDTO> allBlogs() {
+		// TODO Auto-generated method stub
+		List<BlogEntity> blogs=blogRepository.findAll();
+		List<BlogDTO> bDto=new ArrayList<BlogDTO>();
+		for (BlogEntity b : blogs) {
+			BlogDTO blogDto=mapper.map(b, BlogDTO.class);
+			blogDto.setName(b.getDoctor().getName());
+			bDto.add(blogDto);
+		}
+		return bDto;
+	}
+	@Override
+	public Boolean deleteBlog(Long id) {
+		// TODO Auto-generated method stub
+		try {
+		blogRepository.deleteById(id);
+		return true;
+		}catch(Exception e) {
+			return false;
+		}
+	}
+	@Override
+	public BlogDTO updateBlog(Long id, BlogDTO blog) {
+		// TODO Auto-generated method stub
+		BlogEntity b=blogRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Blog Not Found"));
+		b.setContent(blog.getContent());
+		b.setTitle(blog.getTitle());
+		blogRepository.save(b);
+		return blog;
 	}
 
 }
