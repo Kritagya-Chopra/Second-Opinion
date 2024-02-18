0045716f7383ef0c7b8165d98c373e4e459740c7
Kritagya Chopra <er.kritagyachopra@gmail.com>
2024-02-18 18:25:16 +0530

    "Global Exception Handling Functionality

--- Second_Opinion_Java/src/main/java/com/app/exc_handler/GlobalExceptionHandler.java
+++ Second_Opinion_Java/src/main/java/com/app/exc_handler/GlobalExceptionHandler.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.app.custom_exceptions.ResourceNotFoundException;
-//import com.app.dto.ApiResponse;
+import com.app.dto.ResponseDTO;
+
 
 @RestControllerAdvice // =@ControllerAdvice => global exc handler class
 //--common interceptor to intercept ALL excs in all contoller + @ResponseBody added impl. 
@@ -43,21 +44,21 @@ public class GlobalExceptionHandler {
 
 	// method level anno to tell SC , following is an exc handling method : to
 	// handle : ResourceNotFoundException
-//	@ExceptionHandler(ResourceNotFoundException.class)
-//	@ResponseStatus(value = HttpStatus.NOT_FOUND)
-//	public ApiResponse handleResourceNotFoundException(
-//			ResourceNotFoundException e) {
-//		System.out.println("in res not found " + e);
-//		return new ApiResponse(e.getMessage());
-//	}
-//
-//	// method level anno to tell SC , following is an exc handling method : to
-//	// handle any other remaining exc => catch all
-//	@ExceptionHandler(RuntimeException.class)
-//	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-//	public ApiResponse handleAnyException(RuntimeException e) {
-//		System.out.println("in catch-all " + e);
-//		e.printStackTrace();
-//		return new ApiResponse(e.getMessage());
-//	}
+	@ExceptionHandler(ResourceNotFoundException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public ResponseDTO handleResourceNotFoundException(
+			ResourceNotFoundException e) {
+		System.out.println("in res not found " + e);
+		return new ResponseDTO(false,e.getMessage(),"ERROR" , "NOT OK");
+	}
+
+	// method level anno to tell SC , following is an exc handling method : to
+	// handle any other remaining exc => catch all
+	@ExceptionHandler(RuntimeException.class)
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseDTO handleAnyException(RuntimeException e) {
+		System.out.println("in catch-all " + e);
+		e.printStackTrace();
+		return new ResponseDTO(false,e.getMessage(),"ERROR" , "NOT OK");
+	}
 }
