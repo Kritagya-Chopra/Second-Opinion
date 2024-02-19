806c018ce39c845f39821e5ecbf3c2fc096e994c
Kritagya Chopra <er.kritagyachopra@gmail.com>
2024-02-19 11:19:11 +0530

    minor change

--- Second_Opinion_Java/src/main/java/com/app/dto/DiseaseDTO.java
+++ Second_Opinion_Java/src/main/java/com/app/dto/DiseaseDTO.java
@@ -2,6 +2,11 @@ package com.app.dto;
 
 import javax.persistence.Lob;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DiseaseDTO {
 	private String name;
 	private String description;
